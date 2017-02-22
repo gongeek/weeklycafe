0e16447271df79dcb76fbe8f7fddc3b6be8852c7
lz <1244325988@qq.com>
2017-02-22 20:49:13 +0800

    Update:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,24 @@ Site.create(
     url: "http://javascriptweekly.com/",
     recent_item_link: "http://javascriptweekly.com/issues/322",
     rss: "http://javascriptweekly.com/rss/10m068ld")
+
+Site.create(
+    name: "Go Newsletter",
+    url: "http://golangweekly.com/",
+    recent_item_link: "http://golangweekly.com/issues/147",
+    rss: "http://golangweekly.com/rss/185ijbgh")
+Site.create(
+    name: "Mobile Web Weekly",
+    url: "http://mobilewebweekly.co/",
+    recent_item_link: "http://mobilewebweekly.co/issues/145",
+    rss: "http://mobilewebweekly.co/rss/1o9o2fej")
+Site.create(
+    name: "React Status",
+    url: "http://react.statuscode.com/",
+    recent_item_link: "http://react.statuscode.com/issues/24",
+    rss: "http://react.statuscode.com/rss/16782l49")
+Site.create(
+    name: "Web Operations Weekly",
+    url: "http://webopsweekly.com/",
+    recent_item_link: "http://webopsweekly.com/issues/103",
+    rss: "http://webopsweekly.com/rss/1m8b57fh")
