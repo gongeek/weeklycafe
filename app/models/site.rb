e88d0594381a034aaf40ba209ed3e7738994ccb3
宫卫 <gongwei.gw@alibaba-inc.com>
2017-03-11 11:05:47 +0800

    Patch:destroy

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,5 @@
 class Site < ApplicationRecord
-  has_many :item, dependent: :delete
-  has_many :user_site, dependent: :delete
+  has_many :item, dependent: :destroy
+  has_many :user_site, dependent: :destroy
   has_many :user, through: :user_site
 end
